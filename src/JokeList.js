5094d8506336525adfefc2ac9aa9d779fff30e15
kdord <katya.koliada49@gmail.com>
2019-12-18 17:43:37 +0200

    add JokeList component

---
+++ src/JokeList.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class JokeList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Joke List</h1>
+      </div>
+    );
+  }
+}
+export default JokeList;
